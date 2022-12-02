6dd9ab5a234c5528b0ac5294f19cb793dc328c8b
tolgaSan <tolga.sanli@stud.fh-aachen.de>
2022-12-02 13:11:54 +0100

    Update

--- emensa/views/examples/layout/m4_7d_layout.blade.php
+++ emensa/views/examples/layout/m4_7d_layout.blade.php
@@ -6,29 +6,13 @@
 </head>
 <body>
 @section('header')
-    <div id="header">
-        <div id="logo"><img src="img/mensa.png" alt="Mensa Logo" width="80" height="80"></div>
-        <div id="menu">
-            <ul id="reitermenu">
-                <li><a href ="#ankuendigunganker">Ankündigung</a></li>
-                <li><a href ="#koestlichkeitenanker">Speisen</a></li>
-                <li><a href ="#zahlenanker">Zahlen</a></li>
-                <li><a href ="#kontaktanker">Kontakt</a></li>
-                <li><a href ="#wichtigfuerunsanker">Wichtig für uns</a></li>
-            </ul>
-        </div>
-        <div id="irgendwas"> irgendwas </div>
-    </div>
+
     @show
     @section('main')
-        <div>
-            <h3>Dies ist die erste Seite und das ist die Main Section</h3>
-        </div>
+
     @show
     @section('footer')
-        <div>
-            <h4>Footer bereich. Hier sollte Impressum, Copryright und sonstiges stehen.</h4>
-        </div>
+
     @show
 </body>
 </html>
